4ba92a593eb10d1064e50d90ab3a4bd7cf896cfd
srk <senthil.jogi@gmail.com>
2025-01-05 19:38:06 +0100

    Improve logging in initramfs copy script for better debugging

--- 03_copy_initramfs.sh
+++ 03_copy_initramfs.sh
@@ -13,25 +13,27 @@ DESTINATION="pi@srk.local:/tmp/"
 PASSWORD=${1:-$SCP_PASSWORD}
 
 # Copy the file using scp
+echo "1. Copying file to  $DESTINATION"
 sshpass -p $PASSWORD scp $SOURCE_FILE $DESTINATION
 
 # Check if the copy was successful
 if [ $? -eq 0 ];  then
-    echo "File copied successfully to $DESTINATION"
+    echo "2. File copied successfully to $DESTINATION"
     # Delete the content of the NFS folder before extraction
     sshpass -p $PASSWORD ssh pi@srk.local "sudo rm -rf /srv/nfs/*"
     if [ $? -eq 0 ]; then
-        echo "NFS folder content deleted successfully"
+        echo "3. NFS folder content deleted successfully"
+        echo "4. Extracting file to /srv/nfs/"
         # Extract the file in the remote to folder /srv/nfs/
         sshpass -p $PASSWORD ssh pi@srk.local "gunzip -c /tmp/$INPUT_FILENAME | sudo cpio -idmv -D /srv/nfs/"
         if [ $? -eq 0 ]; then
-            echo "File extracted successfully to /srv/nfs/"
+            echo "5. File extracted successfully to /srv/nfs/"
         else
-            echo "Failed to extract file to /srv/nfs/"
+            echo "5. Failed to extract file to /srv/nfs/"
         fi
     else
-        echo "Failed to delete NFS folder content"
+        echo "3. Failed to delete NFS folder content"
     fi
 else
-    echo "Failed to copy file to $DESTINATION"
+    echo "2. Failed to copy file to $DESTINATION"
 fi
