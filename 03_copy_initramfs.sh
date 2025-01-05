838adea9c0d6c7e9547b08901358a681f31978aa
srk <senthil.jogi@gmail.com>
2025-01-05 19:36:29 +0100

    Enhance initramfs copy script and update recipe to integrate new task

--- 03_copy_initramfs.sh
+++ 03_copy_initramfs.sh
@@ -1,22 +1,36 @@
 #!/bin/bash
 
+# Check if the password argument is provided or set as an environment variable
+if [ -z "$1" ] && [ -z "$SCP_PASSWORD" ]; then
+    echo "Usage: $0 <scp_password> or set SCP_PASSWORD environment variable"
+    exit 1
+fi
+
 # Define the input filename and destination
 INPUT_FILENAME="core-image-tiny-initramfs-srk-beaglebone-yocto.cpio.gz"
 SOURCE_FILE="/home/srk2cob/project/poky/build/tmp/deploy/images/beaglebone-yocto/$INPUT_FILENAME"
 DESTINATION="pi@srk.local:/tmp/"
+PASSWORD=${1:-$SCP_PASSWORD}
 
 # Copy the file using scp
-scp $SOURCE_FILE $DESTINATION
+sshpass -p $PASSWORD scp $SOURCE_FILE $DESTINATION
 
 # Check if the copy was successful
 if [ $? -eq 0 ];  then
     echo "File copied successfully to $DESTINATION"
-    # Extract the file in the remote to folder /srv/nfs2/
-    ssh pi@srk.local "gunzip -c /tmp/$INPUT_FILENAME | sudo cpio -idmv -D /srv/nfs3/"
+    # Delete the content of the NFS folder before extraction
+    sshpass -p $PASSWORD ssh pi@srk.local "sudo rm -rf /srv/nfs/*"
     if [ $? -eq 0 ]; then
-        echo "File extracted successfully to /srv/nfs/"
+        echo "NFS folder content deleted successfully"
+        # Extract the file in the remote to folder /srv/nfs/
+        sshpass -p $PASSWORD ssh pi@srk.local "gunzip -c /tmp/$INPUT_FILENAME | sudo cpio -idmv -D /srv/nfs/"
+        if [ $? -eq 0 ]; then
+            echo "File extracted successfully to /srv/nfs/"
+        else
+            echo "Failed to extract file to /srv/nfs/"
+        fi
     else
-        echo "Failed to extract file to /srv/nfs/"
+        echo "Failed to delete NFS folder content"
     fi
 else
     echo "Failed to copy file to $DESTINATION"
