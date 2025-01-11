98ad89d50f30d1e9ab1a9c62bb06e6ad98a31bd6
srk <senthil.jogi@gmail.com>
2025-01-11 01:55:06 +0100

    Enhance BitBake kernel script menu by adding options to print fragment.cfg file path, contents, and workflow

--- 08_bitbake_kernel.sh
+++ 08_bitbake_kernel.sh
@@ -10,8 +10,10 @@ show_menu() {
     echo "6. Print .config file path"
     echo "7. Print defconfig file path meta-srk"
     echo "8. Print defconfig file path"
-    echo "9. Print fragment.cfg path"
-    echo "10. Exit"
+    echo "9. Print fragment.cfg file path"
+    echo "10. Print fragment.cfg file contents"
+    echo "11. Print fragment workflow"
+    echo "12. Exit"
 }
 
 execute_option() {
@@ -41,9 +43,15 @@ execute_option() {
             echo "/home/srk2cob/project/poky/build/tmp/work/beaglebone_yocto-poky-linux-gnueabi/linux-yocto/6.6.21+git/linux-beaglebone_yocto-standard-build/defconfig"
             ;;
         9)
-            cat "/home/srk2cob/project/poky/build/tmp/work/beaglebone_yocto-poky-linux-gnueabi/linux-yocto/6.6.21+git/fragment.cfg"
+            echo "/home/srk2cob/project/poky/build/tmp/work/beaglebone_yocto-poky-linux-gnueabi/linux-yocto/6.6.21+git/fragment.cfg"
             ;;
         10)
+            cat "/home/srk2cob/project/poky/build/tmp/work/beaglebone_yocto-poky-linux-gnueabi/linux-yocto/6.6.21+git/fragment.cfg"
+            ;;
+        11)
+            echo "Run 2. menuconfig.  3. diffconfig and view the fragment.cfg file. using 10. Print fragment.cfg file contents"
+            ;;
+        12)
             echo "Exiting..."
             exit 0
             ;;
