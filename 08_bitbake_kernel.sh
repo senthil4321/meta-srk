57956df865cd1485dd915e7a421febbb889cfc53
srk <senthil.jogi@gmail.com>
2025-01-11 01:50:50 +0100

    Enhance BitBake kernel script menu options and update file path outputs

--- 08_bitbake_kernel.sh
+++ 08_bitbake_kernel.sh
@@ -6,9 +6,12 @@ show_menu() {
     echo "2. bitbake linux-yocto -c menuconfig"
     echo "3. bitbake linux-yocto -c diffconfig"
     echo "4. bitbake linux-yocto -c kernel_configcheck -f"
-    echo "5. Open .config file"
-    echo "6. Open defconfig file"
-    echo "7. Exit"
+    echo "5. Save defconfig"
+    echo "6. Print .config file path"
+    echo "7. Print defconfig file path meta-srk"
+    echo "8. Print defconfig file path"
+    echo "9. Print fragment.cfg path"
+    echo "10. Exit"
 }
 
 execute_option() {
@@ -26,12 +29,21 @@ execute_option() {
             bitbake linux-yocto -c kernel_configcheck -f
             ;;
         5)
-            echo ~/project/poky/build/tmp/work/beaglebone_yocto-poky-linux-gnueabi/linux-yocto/6.6.21+git/linux-beaglebone_yocto-standard-build/.config
+            bitbake linux-yocto -c savedefconfig
             ;;
         6)
-            echo /home/srk2cob/project/poky/meta-srk/recipes-kernel/linux/linux-yocto/defconfig
+            echo "~/project/poky/build/tmp/work/beaglebone_yocto-poky-linux-gnueabi/linux-yocto/6.6.21+git/linux-beaglebone_yocto-standard-build/.config"
             ;;
         7)
+            echo "/home/srk2cob/project/poky/meta-srk/recipes-kernel/linux/linux-yocto/defconfig"
+            ;;
+        8)
+            echo "/home/srk2cob/project/poky/build/tmp/work/beaglebone_yocto-poky-linux-gnueabi/linux-yocto/6.6.21+git/linux-beaglebone_yocto-standard-build/defconfig"
+            ;;
+        9)
+            cat "/home/srk2cob/project/poky/build/tmp/work/beaglebone_yocto-poky-linux-gnueabi/linux-yocto/6.6.21+git/fragment.cfg"
+            ;;
+        10)
             echo "Exiting..."
             exit 0
             ;;
