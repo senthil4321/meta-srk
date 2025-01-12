19fe2b104cf443dbd98fcd1c67bfbebf804f7a14
srk <senthil.jogi@gmail.com>
2025-01-12 19:17:24 +0100

    Enhance 04_copy_zImage.sh to support multiple input files and improve copy operation feedback

--- 04_copy_zImage.sh
+++ 04_copy_zImage.sh
@@ -6,9 +6,9 @@ if [ -z "$1" ] && [ -z "$SCP_PASSWORD" ]; then
     exit 1
 fi
 
-# Define the input filename and destination
-INPUT_FILENAME="zImage"
-SOURCE_FILE="/home/srk2cob/project/poky/build/tmp/deploy/images/beaglebone-yocto/$INPUT_FILENAME"
+# Define the input filenames and destination
+INPUT_FILES=("zImage" "am335x-boneblack.dtb")
+SOURCE_DIR="/home/srk2cob/project/poky/build/tmp/deploy/images/beaglebone-yocto/"
 USERNAME="pi"
 HOSTNAME="srk.local"
 SERVER_NAME="$USERNAME@$HOSTNAME"
@@ -26,23 +26,26 @@ done
 
 PASSWORD=${PASSWORD:-$SCP_PASSWORD}
 
-# Copy the file using scp
-echo "1. Copying $INPUT_FILENAME to $DESTINATION"
-START_TIME=$(date +%s)
-sshpass -p $PASSWORD scp $VERBOSE $SOURCE_FILE $DESTINATION
-# Check if the copy was successful
-if [ $? -eq 0 ];  then
-    echo "2. $INPUT_FILENAME copied successfully to $DESTINATION"
-    # Move the file to the TFTP folder
-    sshpass -p $PASSWORD ssh $VERBOSE $SERVER_NAME "sudo mv /tmp/$INPUT_FILENAME /srv/tftp/"
-    if [ $? -eq 0 ]; then
-        echo "3. $INPUT_FILENAME moved successfully to /srv/tftp/"
+# Copy the files using scp
+for INPUT_FILENAME in "${INPUT_FILES[@]}"; do
+    SOURCE_FILE="$SOURCE_DIR$INPUT_FILENAME"
+    echo "1. Copying $INPUT_FILENAME to $DESTINATION"
+    START_TIME=$(date +%s)
+    sshpass -p $PASSWORD scp $VERBOSE $SOURCE_FILE $DESTINATION
+    # Check if the copy was successful
+    if [ $? -eq 0 ];  then
+        echo "2. $INPUT_FILENAME copied successfully to $DESTINATION"
+        # Move the file to the TFTP folder
+        sshpass -p $PASSWORD ssh $VERBOSE $SERVER_NAME "sudo mv /tmp/$INPUT_FILENAME /srv/tftp/"
+        if [ $? -eq 0 ]; then
+            echo "3. $INPUT_FILENAME moved successfully to /srv/tftp/"
+        else
+            echo "3. Failed to move $INPUT_FILENAME to /srv/tftp/"
+        fi
     else
-        echo "3. Failed to move $INPUT_FILENAME to /srv/tftp/"
+        echo "2. Failed to copy $INPUT_FILENAME to $DESTINATION"
     fi
-else
-    echo "2. Failed to copy $INPUT_FILENAME to $DESTINATION"
-fi
-END_TIME=$(date +%s)
-TIME_TAKEN=$(($END_TIME - $START_TIME))
-echo "Time taken to copy: $TIME_TAKEN seconds"
+    END_TIME=$(date +%s)
+    TIME_TAKEN=$(($END_TIME - $START_TIME))
+    echo "Time taken to copy $INPUT_FILENAME: $TIME_TAKEN seconds"
+done
