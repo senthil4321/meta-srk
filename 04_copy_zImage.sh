84f111eeb40b21283d96729733b9c02617fd70d7
srk <senthil.jogi@gmail.com>
2025-01-12 08:10:12 +0100

    Enhance 04_copy_zImage.sh to support named arguments for password and verbosity

--- 04_copy_zImage.sh
+++ 04_copy_zImage.sh
@@ -2,7 +2,7 @@
 
 # Check if the password argument is provided or set as an environment variable
 if [ -z "$1" ] && [ -z "$SCP_PASSWORD" ]; then
-    echo "Usage: $0 <scp_password> or set SCP_PASSWORD environment variable"
+    echo "Usage: $0 -p <scp_password> or set SCP_PASSWORD environment variable"
     exit 1
 fi
 
@@ -13,17 +13,28 @@ USERNAME="pi"
 HOSTNAME="srk.local"
 SERVER_NAME="$USERNAME@$HOSTNAME"
 DESTINATION="$SERVER_NAME:/tmp/"
-PASSWORD=${1:-$SCP_PASSWORD}
+
+# Parse named arguments
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        -p) PASSWORD="$2"; shift ;;
+        -v) VERBOSE="-v" ;;
+        *) echo "Unknown parameter passed: $1"; exit 1 ;;
+    esac
+    shift
+done
+
+PASSWORD=${PASSWORD:-$SCP_PASSWORD}
 
 # Copy the file using scp
 echo "1. Copying $INPUT_FILENAME to $DESTINATION"
 START_TIME=$(date +%s)
-sshpass -p $PASSWORD scp $SOURCE_FILE $DESTINATION
+sshpass -p $PASSWORD scp $VERBOSE $SOURCE_FILE $DESTINATION
 # Check if the copy was successful
 if [ $? -eq 0 ];  then
     echo "2. $INPUT_FILENAME copied successfully to $DESTINATION"
     # Move the file to the TFTP folder
-    sshpass -p $PASSWORD ssh $SERVER_NAME "sudo mv /tmp/$INPUT_FILENAME /srv/tftp/"
+    sshpass -p $PASSWORD ssh $VERBOSE $SERVER_NAME "sudo mv /tmp/$INPUT_FILENAME /srv/tftp/"
     if [ $? -eq 0 ]; then
         echo "3. $INPUT_FILENAME moved successfully to /srv/tftp/"
     else
