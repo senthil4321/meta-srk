05464a30c5539f95d49934264b48a1bbb4259e26
senthil4321 <senthil.jogi@gmial.com>
2025-01-18 18:09:21 +0100

    Add command-line options to skip seccomp rules for fork and print syscalls

--- recipes-srk/srk-seccomp/files/srk-seccomp.c
+++ recipes-srk/srk-seccomp/files/srk-seccomp.c
@@ -2,12 +2,23 @@
 #include <seccomp.h>
 #include <errno.h>
 #include <unistd.h>
+#include <string.h>
 
 #define VERSION "1.0.0"
 
-int main() {
+int main(int argc, char *argv[]) {
     // Print version
     printf("Version: %s\n", VERSION);
+
+    int skip_fork_seccomp = 0;
+    int skip_print_seccomp = 0;
+    for (int i = 1; i < argc; i++) {
+        if (strcmp(argv[i], "-s") == 0) {
+            skip_fork_seccomp = 1;
+        } else if (strcmp(argv[i], "-p") == 0) {
+            skip_print_seccomp = 1;
+        }
+    }
 
     // Initialize the seccomp filter
     printf("Hello, World! init \n");
@@ -15,8 +26,15 @@ int main() {
     ctx = seccomp_init(SCMP_ACT_KILL); // Default action: kill the process
 
     // Allow the write and usleep syscalls
-    seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(write), 0);
+    if (!skip_print_seccomp) {
+        seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(write), 0);
+    }
     seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(nanosleep), 0);
+
+    if (!skip_fork_seccomp) {
+        // Allow the fork syscall
+        seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(fork), 0);
+    }
 
     // Load the filter
     if (seccomp_load(ctx) < 0) {
