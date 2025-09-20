98618431ca2c20a09cae76de96c25b1c7b550bad
senthil4321 <senthil4321@users.noreply.github.com>
2025-09-20 23:50:50 +0200

    Fix formatting in hello.c by adding newlines to output statements

--- recipes-srk/hello/files/hello.c
+++ recipes-srk/hello/files/hello.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 int main() {
-     printf("Hello, World! from meta-srk layer and recipes-srk V2!!!");
-     printf("Hello, World! 20SEP2025 07:28 !!!");
+     printf("Hello, World! from meta-srk layer and recipes-srk V2!!!\n");
+     printf("Hello, World! 20SEP2025 07:28 !!!\n");
+     printf("Hello, World! 20SEP2025 23:50 !!!\n");
    return 0;
 }
